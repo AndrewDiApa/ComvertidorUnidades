d33059f7ca5dc6c87b346d703bcda0f58abf7a88
Andrew Dianderas Apaza <andrewdiapa09@gmail.com>
2022-09-24 23:13:53 -0500

    conectando funciones y botones

--- app/src/main/java/com/jdlstudios/comvertidordeunidades/MainActivity.kt
+++ app/src/main/java/com/jdlstudios/comvertidordeunidades/MainActivity.kt
@@ -15,54 +15,77 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnCentimetro.setOnClickListener {
+        binding.btnAKm.setOnClickListener {
+            aKilometros()
+        }
+        binding.btnAHm.setOnClickListener {
+            aHectometros()
+        }
+        binding.btnADm.setOnClickListener {
+            aDecametros()
+        }
+        binding.btnAM.setOnClickListener {
+            aMetros()
+        }
+        binding.btnADcm.setOnClickListener {
+            aDecimetros()
+        }
+        binding.btnACcm.setOnClickListener {
             aCentimetros()
         }
-        binding.btnEquivalir.setOnClickListener {
-            aCentimetros()
+        binding.btnAMmm.setOnClickListener {
+            aMilimetros()
         }
+
     }
     fun aKilometros(){
-        val equivalencia_aKilometros:Int
+        val equivalencia_aKilometros: Double
         val metros: Int = binding.editTextMetros.text.toString().toInt()
-        equivalencia_aKilometros = (metros * 1000)
-        binding.equivalencia.text = "$equivalencia_aKilometros"
+        equivalencia_aKilometros = (metros.toDouble() / 1000)
+        binding.labelTransformado.text = "A kilometros"
+        binding.labelResultado.text = "$equivalencia_aKilometros"
     }
     fun aHectometros(){
-        val equivalencia_aHectometros:Int
+        val equivalencia_aHectometros: Double
         val metros: Int = binding.editTextMetros.text.toString().toInt()
-        equivalencia_aHectometros = (metros * 100)
-        binding.equivalencia.text = "$equivalencia_aHectometros"
+        equivalencia_aHectometros = (metros.toDouble() / 100)
+        binding.labelTransformado.text = "A Hectometros"
+        binding.labelResultado.text = "$equivalencia_aHectometros"
     }
     fun aDecametros(){
-        val equivalencia_aDecametros:Int
+        val equivalencia_aDecametros: Double
         val metros: Int = binding.editTextMetros.text.toString().toInt()
-        equivalencia_aDecametros = (metros * 10)
-        binding.equivalencia.text = "$equivalencia_aDecametros"
+        equivalencia_aDecametros = (metros.toDouble() / 10)
+        binding.labelTransformado.text = "A Decametros"
+        binding.labelResultado.text = "$equivalencia_aDecametros"
     }
     fun aMetros(){
-        val equivalencia_aMetros:Int
+        val equivalencia_aMetros: Double
         val metros: Int = binding.editTextMetros.text.toString().toInt()
-        equivalencia_aMetros = (metros * 1)
-        binding.equivalencia.text = "$equivalencia_aMetros"
+        equivalencia_aMetros = (metros.toDouble() * 1)
+        binding.labelTransformado.text = "A metros"
+        binding.labelResultado.text = "$equivalencia_aMetros"
     }
     fun aDecimetros(){
         val equivalencia_aDecimetros: Double
         val metros: Int = binding.editTextMetros.text.toString().toInt()
-        equivalencia_aDecimetros = (metros.toDouble() / 10)
-        binding.equivalencia.text = "La equivalencia es $equivalencia_aDecimetros"
+        equivalencia_aDecimetros = (metros.toDouble() * 10)
+        binding.labelTransformado.text = "A Decimetros"
+        binding.labelResultado.text = "$equivalencia_aDecimetros"
     }
     fun aCentimetros(){
         val equivalencia_aCentimetros: Double
         val metros: Int = binding.editTextMetros.text.toString().toInt()
-        equivalencia_aCentimetros = (metros.toDouble() / 100)
-        binding.equivalencia.text = "La equivalencia es $equivalencia_aCentimetros"
+        equivalencia_aCentimetros = (metros.toDouble() * 100)
+        binding.labelTransformado.text = "A Centimetros"
+        binding.labelResultado.text = "$equivalencia_aCentimetros"
     }
     fun aMilimetros(){
         val equivalencia_aMilimetros: Double
         val metros: Int = binding.editTextMetros.text.toString().toInt()
-        equivalencia_aMilimetros = (metros.toDouble() / 1000)
-        binding.equivalencia.text = "La equivalencia es $equivalencia_aMilimetros"
+        equivalencia_aMilimetros = (metros.toDouble() * 1000)
+        binding.labelTransformado.text = "A Milimetros"
+        binding.labelResultado.text = "$equivalencia_aMilimetros"
     }
 
 }
